3138276930cb57c31d4e3ce9acc164586b609943
TsoukalasIgnatios <tsouk.ig@gmail.com>
2021-02-02 17:49:12 +0200

    change .gitignore

--- src/test/java/com/movie/directory/movies/demo/test/ApplicationTests.java
+++ src/test/java/com/movie/directory/movies/demo/test/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.book.directory.books.demo;
+package com.movie.directory.movies.demo.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
